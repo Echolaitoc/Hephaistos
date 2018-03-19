1a048364d9d51c06ff80f76f78a8626943d97cef
Tristan Weber <echo1990@gmx.de>
2018-03-19 19:18:00 +0100

    fix InstantiatePrefab, add Destroy
    works in edit and play mode

--- Hephaistos.cs
+++ Hephaistos.cs
@@ -5,9 +5,23 @@ public static class Hephaistos
     public static Object InstantiatePrefab(Object original)
     {
 #if UNITY_EDITOR
-        return UnityEditor.PrefabUtility.InstantiatePrefab(original);
-#else
-        return Instantiate(original);
+        if (!Application.isPlaying)
+        {
+            return UnityEditor.PrefabUtility.InstantiatePrefab(original);
+        }
 #endif
+        return GameObject.Instantiate(original);
+    }
+
+    public static void Destroy(Object obj)
+    {
+#if UNITY_EDITOR
+        if (!Application.isPlaying)
+        {
+            UnityEngine.Object.DestroyImmediate(obj);
+            return;
+        }
+#endif
+        UnityEngine.Object.Destroy(obj);
     }
 }

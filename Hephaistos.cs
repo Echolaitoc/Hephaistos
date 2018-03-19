4129018559057d5c5fb846ab88e6819588abc8e4
Tristan Weber <echo1990@gmx.de>
2018-03-19 13:24:37 +0100

    Initial commit

---
+++ Hephaistos.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class Hephaistos
+{
+    private static Object InstantiatePrefab(Object original)
+    {
+#if UNITY_EDITOR
+        return UnityEditor.PrefabUtility.InstantiatePrefab(original);
+#else
+        return Instantiate(original);
+#endif
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Just my utility functions in a repo to easily reuse across projects.
+Nothing special to see here. Move along.
